01d9ce6883918f4606b0b4740e1b915b8c56b13d
Ashley Ginsberg <aginsberg@mdlive.com>
2021-01-25 22:16:23 -0500

    Revert changes to ButtonLink

--- src/components/ButtonLink.tsx
+++ src/components/ButtonLink.tsx
@@ -1,15 +1,16 @@
 import styled from 'styled-components';
-import { space, color, SpaceProps, ColorProps } from 'styled-system';
+import { space, SpaceProps } from 'styled-system';
 import { bold, align } from './Text';
+import { hyperlink } from './Hyperlink';
 
 export type ButtonLinkProps = {
   bold?: boolean;
   id?: string;
   align?: 'center' | 'left' | 'right';
+  textColor?: string;
   type?: string;
   onClick?: (e: any) => any;
-} & SpaceProps &
-  ColorProps;
+} & SpaceProps;
 
 export const ButtonLink = styled.button<ButtonLinkProps>`
   background-color: ${({ theme }) => theme.colors.transparent};
@@ -18,7 +19,6 @@ export const ButtonLink = styled.button<ButtonLinkProps>`
   padding: 0;
 
   cursor: pointer;
-  text-decoration: none;
 
   &:hover {
     text-decoration: underline;
@@ -27,14 +27,14 @@ export const ButtonLink = styled.button<ButtonLinkProps>`
   ${bold}
   ${align}
 
-  ${color}
+  ${hyperlink}
+
   ${space}
 `;
 
 ButtonLink.displayName = 'ButtonLink';
 ButtonLink.defaultProps = {
   bold: true,
-  color: 'primary',
 };
 
 export default ButtonLink;
