e3de4156ba4c883a0906f8345acc9c810bd948a4
Jorge Adrian Garcia Guerra <nsl-jgarcia@Jorges-MacBook-Pro.local>
2021-02-02 16:02:24 -0600

    Changing font-weight of labels next to radio buttons

---
+++ stories/RadioField.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { withKnobs, boolean } from '@storybook/addon-knobs';
+import RadioField from '../src/components/forms/RadioField';
+
+export default {
+  title: 'RadioField',
+  component: RadioField,
+};
+
+export const RadioFieldStorie = () => (
+  <RadioField
+    id="myRadioField"
+    label="Label test"
+    disabled={boolean('disabled', false)}
+  />
+);
