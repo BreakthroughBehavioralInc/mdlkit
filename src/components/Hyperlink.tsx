b89ba946302a5b09b8aed284a2231fe53baad87a
Ashley Ginsberg <aginsberg@mdlive.com>
2021-02-11 13:02:05 -0500

    Test fix for hyperlink

--- src/components/Hyperlink.tsx
+++ src/components/Hyperlink.tsx
@@ -1,11 +1,15 @@
 import styled, { css } from 'styled-components';
 import { themeGet, space, SpaceProps } from 'styled-system';
-import { bold, align } from './Text';
 
 export const hyperlink = css<{ textColor?: string }>`
   color: ${({ textColor }) => textColor || themeGet('colors.primary')};
   text-decoration: none;
 `;
+
+export const align = props => (props.align ? { textAlign: props.align } : null);
+
+export const bold = props =>
+  props.bold ? { fontWeight: props.theme.fontWeights.bold } : null;
 
 export type HyperlinkProps = {
   bold?: boolean;
