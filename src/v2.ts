c1ef4ab03a686af1bfba56ccdeab9884ebb320ae
Zachary Namyat <znamyat@mdlive.com>
2025-10-26 15:34:01 -0500

    V2 Tooltip init

--- src/v2.ts
+++ src/v2.ts
@@ -1,1 +1,2 @@
-export { default } from './components/v2/Button/Index';
+export { default as Button } from './components/v2/Button/Index';
+export { default as Tooltip } from './components/v2/Tooltip/Index';
