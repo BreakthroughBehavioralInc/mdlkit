4de6ba75a5b70bccc708f5513564a455dc35a692
Zachary Namyat <znamyat@mdlive.com>
2025-12-02 00:23:34 -0600

    v0.4.0-alpha.5

--- src/index.ts
+++ src/index.ts
@@ -66,3 +66,19 @@ export { default as createColorStyles } from './utils/createColorStyles';
 
 // Icons
 export { default as Svg } from './icons/Svg';
+
+// v2
+export { default as ButtonV2 } from './components/v2/Button/Index';
+export { default as ChipV2 } from './components/v2/Chip/Index';
+export { default as DatePickerV2 } from './components/v2/DatePicker/Index';
+export { default as FlexV2 } from './components/v2/Flex/Index';
+export { default as InfoBoxV2 } from './components/v2/InfoBox/Index';
+export { Input as InputV2 } from './components/v2/Input/Index';
+export { default as LoadingSpinnerV2 } from './components/v2/LoadingSpinner/Index';
+export { default as TooltipV2 } from './components/v2/Tooltip/Index';
+export {
+  Accordion as AccordionV2,
+  AccordionItem as AccordionItemV2,
+  AccordionTrigger as AccordionTriggerV2,
+  AccordionContent as AccordionContentV2,
+} from './components/v2/Accordion/Index';
