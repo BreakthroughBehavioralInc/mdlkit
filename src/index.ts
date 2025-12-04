649ce506d77f4f1b09ed20c146245cb94f01db49
Zachary Namyat <znamyat@mdlive.com>
2025-12-03 23:38:53 -0600

    v0.4.0-alpha.9

--- src/index.ts
+++ src/index.ts
@@ -70,6 +70,7 @@ export { default as Svg } from './icons/Svg';
 // v2
 export { default as ButtonV2 } from './components/v2/Button/Index';
 export { default as ChipV2 } from './components/v2/Chip/Index';
+export { default as PillV2 } from './components/v2/Pill/Index';
 export { default as DatePickerV2 } from './components/v2/DatePicker/Index';
 export { default as FlexV2 } from './components/v2/Flex/Index';
 export { default as InfoBoxV2 } from './components/v2/InfoBox/Index';
